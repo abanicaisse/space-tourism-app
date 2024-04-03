dd5aa266c644db6e1f3e2843221cb4112e693e13
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-04-03 23:37:29 +0300

    Fixed bug with Navbar & App Structure

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import "./home.css";
+import { Navbar } from "../../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Navbar />
+
+      <div className="home__container .component-maxwidth">
+        <div className="home__container-text">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="home__container-explore">
+          <p>EXPLORE</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
+
+// import React from "react";
+// import "./home.css";
+// import { Navbar } from "../../components";
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <Navbar />
+
+//     </div>
+//   );
+// };
+
+// export default Home;
