421f397f91fa02f971ee4d9e94192d59af8ce3d6
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-05-05 18:24:11 +0300

    added //crew

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/crew" element={<Crew />} />
+        <Route path="//crew" element={<Crew />} />
         <Route path="/destination" element={<Destination />} />
         <Route path="/technology" element={<Technology />} />
       </Routes>
